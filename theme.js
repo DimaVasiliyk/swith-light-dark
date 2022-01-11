cbcdd45cd370cbabd69e54f8e813d7e476dbad97
DimaVasiliyk <dmytro.vasyliuk1@gmail.com>
2022-01-11 17:22:56 +0200

    Saas

--- theme.js
+++ theme.js
@@ -1,10 +1,6 @@
-let swichMode = document.getElementById("switchMode")
+const html = document.querySelector('html');
+html.dataset.theme = `theme-light`;
 
-swichMode.onclick = function(){
-    let theme   = document.getElementById("theme")
-    if(theme.getAttribute('href') == "theme-light.css"){
-        theme.href = "theme-dark.css"
-    }else {
-        theme.href = "theme-light.css"
-    }
+function switchTheme(theme) {
+  html.dataset.theme = `theme-${theme}`;
 }
