631ea0e401a6a5c15051e2bfac909c4628e2d5c8
DimaVasiliyk <dmytro.vasyliuk1@gmail.com>
2022-01-11 18:12:35 +0200

    S

--- theme.js
+++ theme.js
@@ -1,6 +1,11 @@
-const html = document.querySelector('html');
-html.dataset.theme = `theme-light`;
+const themeSwitcherButton = document.querySelector('.js-theme-switcher')
 
-function switchTheme(theme) {
-  html.dataset.theme = `theme-${theme}`;
-}
+themeSwitcherButton.addEventListener('click', function() {
+    const body = document.querySelector('body')
+  
+  if (body.classList.contains('dark-theme')) {
+    body.classList.remove('dark-theme')
+  } else {
+    body.classList.add('dark-theme')
+  }
+})
