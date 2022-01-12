8265ff68bea5ce6f721b6ebc37010db1914bf076
DimaVasiliyk <dmytro.vasyliuk1@gmail.com>
2022-01-12 17:06:10 +0200

    Add mixin

--- theme.js
+++ theme.js
@@ -1,11 +1,13 @@
-const themeSwitcherButton = document.querySelector('.js-theme-switcher')
+const themeSwitcherButton = document.querySelector('.changes')
 
 themeSwitcherButton.addEventListener('click', function() {
     const body = document.querySelector('body')
   
   if (body.classList.contains('dark-theme')) {
     body.classList.remove('dark-theme')
+    body.classList.add('light-theme')
   } else {
     body.classList.add('dark-theme')
+    body.classList.remove('light-theme')
   }
 })
